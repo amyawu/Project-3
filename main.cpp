017bc02f795624befe7160cc6425153117c6d9fb
Karen Liu <73561226+KareO2@users.noreply.github.com>
2022-04-20 22:45:39 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,18 +3,24 @@
 #include <unordered_map>
 #include <vector>
 #include <fstream>
+#include <sstream>
 #include <chrono>
 #include <algorithm>
+#include <string>
 using namespace std;
 using namespace chrono;
 
 class Graph {
-    unordered_map<string, unsigned int> mappy;
+    unordered_map<string, int> mappy;
 
 public:
+    Graph();
     void putIn(string category);
-    vector<pair<unsigned int, string>> organize();
+    vector<pair<int, string>> organize();
 };
+
+Graph::Graph() {
+}
 
 void Graph::putIn(string category) {
     if (mappy.count(category) > 0) {
@@ -25,9 +31,29 @@ void Graph::putIn(string category) {
     }
 }
 
-vector<pair<unsigned int, string>> Graph::organize() {
-    vector<pair<unsigned int, string>> ordering;
+vector<pair<int, string>> Graph::organize() {
+    vector<pair<int, string>> ordering;
     for(auto iter = mappy.begin(); iter != mappy.end(); iter++) {
+        if (iter->first.at(0) == '"') {
+            if (iter->first.at(1) == 'M') {
+                int num = iter->second;
+                string first = "Money transfer, virtual currency, or money service";
+                ordering.push_back(make_pair(num,first));
+                continue;
+            }
+            else if (iter->first.at(1) == 'C') {
+                int num = iter->second;
+                string first = "Credit reporting, credit repair services, or other personal consumer reports";
+                ordering.push_back(make_pair(num,first));
+                continue;
+            }
+            else if (iter->first.at(1) == 'P') {
+                int num = iter->second;
+                string first = "Payday loan, title loan, or personal loan";
+                ordering.push_back(make_pair(num,first));
+                continue;
+            }
+        }
         ordering.push_back(make_pair(iter->second,iter->first));
     }
     sort(ordering.rbegin(), ordering.rend());
@@ -35,7 +61,7 @@ vector<pair<unsigned int, string>> Graph::organize() {
 }
 
 //opening and reading file
-void ReadFile(Graph unorderedMappy) {
+void ReadFile(Graph& unorderedMappy) {
     ifstream info("rows.csv");
     if (info.is_open()) {
         string liner;
@@ -66,12 +92,17 @@ int main() {
 
     Graph unorderedMappy;
     ReadFile(unorderedMappy);
-    vector<pair<unsigned int, string>> ordered = unorderedMappy.organize();
+    vector<pair<int, string>> ordered = unorderedMappy.organize();
 
-    cout << "The most common complaint is:" << ordered[0].second << endl;
+    int size = ordered.size();
+
+    cout << "The most common complaint is: " << ordered[0].second << endl;
+    cout << "With " << ordered[0].first << " calls" << endl;
+    cout << "The least common complaint is: " << ordered[size-1].second << endl;
+    cout << "With " << ordered[size-1].first << " calls" << endl;
 
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
-    cout << "Runtime:" << duration.count() << endl;
+    cout << "Runtime for Unordered Map: " << duration.count() << endl;
     return 0;
 }
